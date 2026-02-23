1f509a84dcc8a9bd9432c0eb63e56ddb7f56c5b6
saikou813 <saiksuso@gmail.com>
2026-02-23 04:24:46 +0000

    fix: conditional ssl for sqlite compatibility

--- codestar/settings.py
+++ codestar/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
     'default': dj_database_url.config(
         default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
         conn_max_age=600,
-        ssl_require=True
+        ssl_require=True if os.environ.get('DATABASE_URL') else False
     )
 }
 
