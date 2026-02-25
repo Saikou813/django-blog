aba6c9b0d3baa79d40705a5bb5997aac67c26f1d
saikou813 <saiksuso@gmail.com>
2026-02-25 02:28:46 +0000

    python version change and building the post model

--- codestar/settings.py
+++ codestar/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
@@ -92,7 +92,10 @@ DATABASES = {
 )
 }
 
-
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeinstitute-ide.net/",
+    "https://*.herokuapp.com"
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
