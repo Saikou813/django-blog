5efac73aa0b9859aeb48510ef0070a6c5dcf282f
saikou813 <saiksuso@gmail.com>
2026-02-28 01:39:33 +0000

    add Comment, Event, and Ticket models with admin customisation

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
 # Register your models here.
 admin.site.register(Post)
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('author', 'body', 'post', 'created_on', 'approved')
+    list_filter = ('approved', 'created_on')
+    search_fields = ('author__username', 'body')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
